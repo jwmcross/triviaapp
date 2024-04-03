985e4e6b79649bd68a33400e48ec27a9c1b0c379
John Cross <hello@johncross.dev>
2024-04-03 19:05:04 +0100

    init

---
+++ src/main/java/com/johncross/demo/santanderinterview/SantanderInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.johncross.demo.santanderinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SantanderInterviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/johncross/demo/santanderinterview/SantanderInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johncross.demo.santanderinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
